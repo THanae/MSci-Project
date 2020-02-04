3d668d56264f5b3bf0bd1e40e0af74280d1199a4
THanae <hanae188@outlook.com>
2020-02-04 20:20:17 +0000

    cleaned add_branches()

--- data_loader.py
+++ data_loader.py
@@ -5,11 +5,11 @@ from typing import List
 
 
 def load_data(columns: List[str]):
-    """
+    '''
     Load data sets with the specified column names
     :param columns: columns to load
     :return:
-    """
+    '''
     columns = set(columns)
     file_names = ['Lb2pKmumu_2018_MagUp.root', 'Lb2pKmumu_2018_MagDown.root', 'Lb2pKmumu_2017_MagUp.root',
                   'Lb2pKmumu_2017_MagDown.root']
@@ -35,15 +35,20 @@ def add_branches():
     """
     lb = ['Lb_M', 'Lb_ENDVERTEX_CHI2', 'Lb_DIRA_OWNPV', 'Lb_M', 'Lb_FD_OWNPV', 'Lb_OWNPV_X', 'Lb_OWNPV_Y', 'Lb_OWNPV_Z',
           'Lb_OWNPV_XERR', 'Lb_OWNPV_YERR', 'Lb_OWNPV_ZERR', 'Lb_ENDVERTEX_X', 'Lb_ENDVERTEX_Y', 'Lb_ENDVERTEX_Z',
-          'Lb_ENDVERTEX_XERR', 'Lb_ENDVERTEX_YERR', 'Lb_ENDVERTEX_ZERR', "Lb_PE", "Lb_PX", "Lb_PY", "Lb_PZ", "Lb_P",
-          'Lb_L0Global_Dec', 'mu1_L0Global_Dec', 'tauMu_L0Global_Dec', 'Lb_FDCHI2_OWNPV']
+          'Lb_ENDVERTEX_XERR', 'Lb_ENDVERTEX_YERR', 'Lb_ENDVERTEX_ZERR', 'Lb_PE', 'Lb_PX', 'Lb_PY', 'Lb_PZ', 'Lb_P',
+          'Lb_L0Global_Dec', 'Lb_FDCHI2_OWNPV']
+    lb_pmu = ['Lb_pmu_TR1_PIDp', 'Lb_pmu_TR1_PIDK', 'Lb_pmu_TR1_PIDmu', 'Lb_pmu_TR1_PIDpi']
     pkmu = ['pKmu_P', 'pKmu_ENDVERTEX_X', 'pKmu_ENDVERTEX_Y', 'pKmu_ENDVERTEX_Z', 'pKmu_OWNPV_X', 'pKmu_OWNPV_Y',
-            'pKmu_OWNPV_Z', 'pKmu_PE', 'pKmu_PX', 'pKmu_PY', 'pKmu_PZ']
-    cleaning = ["proton_P", "proton_PT", "Kminus_PT", "mu1_P", "mu1_PT", "tauMu_P", "tauMu_PT"]
-    proton = ["proton_PE", "proton_PX", "proton_PY", "proton_PZ", "proton_P"]
-    kminus = ["Kminus_PE", "Kminus_PX", "Kminus_PY", "Kminus_PZ", "Kminus_P"]
-    mu1 = ["mu1_PE", "mu1_PX", "mu1_PY", "mu1_PZ", "mu1_P", 'mu1_REFPX', 'mu1_REFPY', 'mu1_REFPZ']
-    mu2 = ["tauMu_PE", "tauMu_PX", "tauMu_PY", "tauMu_PZ", "tauMu_P", 'tauMu_REFPX', 'tauMu_REFPY', 'tauMu_REFPZ']
+            'pKmu_OWNPV_Z', 'pKmu_PE', 'pKmu_PX', 'pKmu_PY', 'pKmu_PZ', 'pKmu_PT', 'pKmu_OWNPV_CHI2',
+            'pKmu_ENDVERTEX_CHI2']
+    proton = ['proton_PE', 'proton_PX', 'proton_PY', 'proton_PZ', 'proton_P', 'proton_REFPX', 'proton_REFPY',
+              'proton_REFPZ', 'proton_PT']
+    kminus = ['Kminus_PE', 'Kminus_PX', 'Kminus_PY', 'Kminus_PZ', 'Kminus_P', 'Kminus_REFPX', 'Kminus_REFPY',
+              'Kminus_REFPZ', 'Kminus_PT']
+    mu1 = ['mu1_PE', 'mu1_PX', 'mu1_PY', 'mu1_PZ', 'mu1_P', 'mu1_REFPX', 'mu1_REFPY', 'mu1_REFPZ', 'mu1_L0Global_Dec',
+           'mu1_PT', 'mu1_isMuon']
+    mu2 = ['tauMu_PE', 'tauMu_PX', 'tauMu_PY', 'tauMu_PZ', 'tauMu_P', 'tauMu_REFPX', 'tauMu_REFPY', 'tauMu_REFPZ',
+           'tauMu_L0Global_Dec', 'tauMu_PT', 'tauMu_isMuon']
     # error_pkmu_ref = ['pKmu_REFP_COVXX', 'pKmu_REFP_COVYY', 'pKmu_REFP_COVZZ', 'pKmu_REFP_COVXY', 'pKmu_REFP_COVXZ',
     #                   'pKmu_REFP_COVYZ']
     # error_taumu = ['tauMu_REFP_COVXX', 'tauMu_REFP_COVYY', 'tauMu_REFP_COVZZ', 'tauMu_REFP_COVXY', 'tauMu_REFP_COVXZ',
@@ -54,13 +59,11 @@ def add_branches():
     #          ['XX', 'YY', 'ZZ', 'XY', 'XZ', 'YZ']]
     # errors = error_pkmu_ref + error_taumu + p_cov + p_ref_cov
     errors = []
-    chi_squared = ['pKmu_OWNPV_CHI2', 'mu1_isMuon', 'pKmu_ENDVERTEX_CHI2']
-    impact_parameter = ['proton_IPCHI2_OWNPV', 'Kminus_IPCHI2_OWNPV', 'mu1_IPCHI2_OWNPV', 'tauMu_IPCHI2_OWNPV',
-                        'pKmu_IPCHI2_OWNPV']
+    ip = ['proton_IPCHI2_OWNPV', 'Kminus_IPCHI2_OWNPV', 'mu1_IPCHI2_OWNPV', 'tauMu_IPCHI2_OWNPV', 'pKmu_IPCHI2_OWNPV']
     pid = ['proton_PIDe', 'Kminus_PIDe', 'Kminus_PIDK', 'Kminus_PIDmu', 'proton_PIDK', 'proton_PIDp', 'proton_PIDmu',
            'proton_PIDp', 'Kminus_PIDK', 'mu1_PIDmu', 'tauMu_PIDmu', 'proton_PIDK', 'proton_PIDmu', 'proton_PIDd',
            'Kminus_PIDp', 'Kminus_PIDmu', 'mu1_PIDp', 'mu1_PIDK', 'tauMu_PIDp', 'tauMu_PIDK', 'Kminus_PIDd']
-    bdt = ['Lb_pmu_ISOLATION_BDT1', 'Lb_pmu_ISOLATION_BDT2', 'Lb_pmu_ISOLATION_BDT3', 'Lb_pmu_ISOLATION_BDT4']
+    isolation = ['Lb_pmu_ISOLATION_BDT1', 'Lb_pmu_ISOLATION_BDT2', 'Lb_pmu_ISOLATION_BDT3', 'Lb_pmu_ISOLATION_BDT4']
     mc = ['Lb_MC_MOTHER_ID', 'pKmu_MC_MOTHER_ID', 'proton_MC_MOTHER_ID', 'mu1_MC_MOTHER_ID', 'tauMu_MC_MOTHER_ID',
           'Kminus_MC_MOTHER_ID', 'Lb_MC_GD_MOTHER_ID', 'proton_MC_GD_MOTHER_ID', 'mu1_MC_GD_MOTHER_ID',
           'Kminus_MC_GD_MOTHER_ID', 'tauMu_MC_GD_MOTHER_ID', 'Kminus_ID', 'proton_ID', 'mu1_ID', 'tauMu_ID',
@@ -72,8 +75,7 @@ def add_branches():
     mc = ['mu1_ID', 'tauMu_ID', 'Kminus_ID', 'proton_ID']
     # true = ['pKmu_TRUEENDVERTEX_X', 'pKmu_TRUEENDVERTEX_Y', 'pKmu_TRUEENDVERTEX_Z', 'tauMu_TRUEP_X', 'tauMu_TRUEP_Y',
     #         'tauMu_TRUEP_Z', 'pKmu_TRUEP_X', 'pKmu_TRUEP_Y', 'pKmu_TRUEP_Z']
-    pt = ['pKmu_PT', 'mu1_PT']
-    return lb + cleaning + proton + kminus + mu1 + mu2 + pkmu + errors + chi_squared + impact_parameter + pid + bdt + mc + pt + ['Lb_pmu_TR1_PIDp', 'Lb_pmu_TR1_PIDK', 'Lb_pmu_TR1_PIDmu', 'Lb_pmu_TR1_PIDpi']
+    return lb + proton + kminus + mu1 + mu2 + pkmu + errors + ip + pid + isolation + mc + lb_pmu
 
 
 def check_for_both_charges(data_frame):
