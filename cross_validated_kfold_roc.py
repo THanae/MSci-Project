a7582fab4b5ead27080b0538ae7345d86aeb9210
THanae <hanae188@outlook.com>
2020-03-08 14:59:09 +0000

    .

--- cross_validated_kfold_roc.py
+++ cross_validated_kfold_roc.py
@@ -4,21 +4,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn import svm, datasets
-from sklearn.metrics import plot_roc_curve, roc_curve, accuracy_score, confusion_matrix, auc
+from sklearn.metrics import plot_roc_curve, roc_curve, auc
 from sklearn.model_selection import StratifiedKFold
 
 from mva import get_x_and_y
 
+# np.random.seed(5521)
+
 X, y = get_x_and_y()
+# background = len(pd.read_csv('cleaned_data_background2.csv'))
+# background = len(pd.read_csv('cleaned_data_background_from_7000mev.csv'))
+# background = len(pd.read_csv('cleaned_data_background_from_7000mev_protonpid25.csv'))
+# background = len(pd.read_csv('cleaned_data_background_from_3sigma_protonpid25.csv'))
+# background = len(pd.read_csv('cleaned_data_background_from_7000mev_protonpid25_morefeatures.csv'))
+# signal = len(pd.read_csv('b_mc_data2.csv')) * 0.0023072060793360356
+# signal = len(pd.read_csv('b_mc_data2.csv')) * 0.0008931248183174746
+# signal = len(pd.read_csv('b_mc_data2.csv')) * 0.0023072060793360356
+# signal = 120
+# background = 2458 - 120
+signal = 62
+background = 769 - 62
+print(signal, background)
 cv = StratifiedKFold(n_splits=3)
 bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2), algorithm="SAMME", n_estimators=500, learning_rate=0.01)
 tprs, aucs = [], []
-mean_fpr = np.linspace(0, 1, 100)
+mean_fpr = np.linspace(0, 1, 100000)
 mean_tpr = 0
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 for i, (train, test) in enumerate(cv.split(X, y)):
     # print(i)
     # y_train = y[train]
@@ -38,15 +51,21 @@ for i, (train, test) in enumerate(cv.split(X, y)):
     # tprs.append(interp_tpr)
     # aucs.append(viz.roc_auc)
 
-    bdt.fit(X.loc[train], y[train])
-    probas_ = bdt.predict_proba(X.loc[test])
-    fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+    sample_weight = np.zeros_like(y[train])
+    sample_weight[y[train] == 0] = 1 / (y[train] == 0).sum()
+    sample_weight[y[train] == 1] = 1 / (y[train] == 1).sum()
+    bdt.fit(X.loc[train], y[train], sample_weight=sample_weight)
+    prob = bdt.predict_proba(X.loc[test])
+    sample_weight = np.zeros_like(y[test])
+    sample_weight[y[test] == 0] = 1 / (y[test] == 0).sum()
+    sample_weight[y[test] == 1] = 1 / (y[test] == 1).sum()
+    fpr, tpr, thresholds = roc_curve(y[test], prob[:, 1], sample_weight=sample_weight)
     mean_tpr += interp(mean_fpr, fpr, tpr)
     print(mean_tpr)
     mean_tpr[0] = 0.0
     roc_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
-    plt.plot(fpr, thresholds, label='threshold fold %d' % i)
+    ax1.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+    ax1.plot(fpr, thresholds, label='threshold fold %d' % i)
     interp_tpr = interp(mean_fpr, fpr, tpr)
     interp_tpr[0] = 0.0
     tprs.append(interp_tpr)
@@ -58,12 +77,17 @@ mean_tpr = np.mean(tprs, axis=0)
 print(mean_tpr)
 mean_tpr[-1] = 1.0
 mean_auc, std_auc = auc(mean_fpr, mean_tpr), np.std(aucs)
-ax.plot(mean_fpr, mean_tpr, color='b', label=r'Mean ROC (AUC = %0.2f$\pm$%0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
+ax1.plot(mean_fpr, mean_tpr, color='b', label=r'Mean ROC (AUC = %0.2f$\pm$%0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
 std_tpr = np.std(tprs, axis=0)
 tprs_upper, tprs_lower = np.minimum(mean_tpr + std_tpr, 1), np.maximum(mean_tpr - std_tpr, 0)
-ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
+ax1.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
 
-ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], title="Receiver operating characteristic curve")
-ax.legend(loc="lower right")
+# ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], title="Receiver operating characteristic curve")
+ax1.legend(loc="lower right")
+sig = signal * np.array(mean_tpr) / np.sqrt(signal * np.array(mean_tpr) + background * np.array(mean_fpr))
+print(sig)
+ax1.axvline(mean_fpr[np.nanargmax(sig[1:])+1])
+ax2.plot(mean_fpr, sig, color='k', label=r'Signal significance', lw=2, alpha=.8)
+ax2.legend(loc="lower right")
+ax2.axvline(mean_fpr[np.nanargmax(sig)])
 plt.show()
-
